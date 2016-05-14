73dcd11392a99c9c06d66cc165e924981e5de4b2
Randy Marmer <rmarmer1@yahoo.com>
2016-05-14 18:16:47 -0400

    ch10; sort.rb - Work-in-progress

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,35 @@
-def sort arr
-  # your code here
+def sort words
+
+recursive_sort words, []
+
 end
+
+def recursive_sort words, sorted
+
+	yet_to_sort = []
+
+	sorted.length.upto(words.length) do
+
+		word_a = words.pop
+
+		words.each do |word_b|
+
+			if word_b < word_a  
+
+				yet_to_sort << word_a
+
+				word_a = word_b
+
+			else
+
+				yet_to_sort << word_b
+			end
+		end
+		sorted << word_a
+
+	recursive_sort yet_to_sort, sorted
+end
+puts sorted
+end
+
+puts sort(['rubber','baby','buggy','bumpers'])

9b3da0a07e1ee5c1bfceab5c4186c0a8f6c50d2a
Randy Marmer <rmarmer1@yahoo.com>
2016-05-15 00:25:39 -0400

    ch10; dictionary_sort.rb - Work-in-progress

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,35 @@
 def dictionary_sort arr
-  # your code here
+
+recursive_sort words, []
+
 end
+
+def recursive_sort words, sorted
+
+	yet_to_sort = []
+
+	sorted.length.upto(words.length) do
+
+		word_a = words.pop
+
+		words.each do |word_b|
+
+			if word_b.downcase < word_a.downcase  
+
+				yet_to_sort << word_a
+
+				word_a = word_b
+
+			else
+
+				yet_to_sort << word_b
+			end
+		end
+		sorted << word_a
+
+	recursive_sort yet_to_sort, sorted
+end
+puts sorted
+end
+
+puts sort(['rubber','baby','A','buggy','bumper'])

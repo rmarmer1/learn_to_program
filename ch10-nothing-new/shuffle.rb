f60c17b2dfde2db070e1fef8932405f555543a9e
Randy Marmer <rmarmer1@yahoo.com>
2016-05-14 23:58:15 -0400

    ch10; shuffle.rb - Work-in-progress

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,16 @@
-def shuffle arr
-  # your code here
+#myArray = (1..9).to_a
+
+#myArray.sample(9)
+#myArray = [1,2,3,4,5,6,7,8,9]
+
+#puts myArray
+
+# this works, but not if the array itself is passed in... ?!?!?!?
+
+def shuffle(range)
+  
+  range.to_a.sample(9)
+
 end
+#
+shuffle(1..9)

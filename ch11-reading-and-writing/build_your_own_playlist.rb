8337a25a8ca8bcece4829276aa21148d12e42744
Randy Marmer <rmarmer1@yahoo.com>
2016-05-16 13:32:59 -0400

    ch11; build_your_own_playlist.rb - Work-in-progress

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
-# your code here
+
+m4as = Dir['/Users/admin/Downloads/Precourse/TakeOne/*.m4a']
+puts m4as
+m4as = m4as.sample(m4as.length)
+File.open 'playlist.m4a', 'w' do |f|
+
+m4as.each do |m4a|
+
+f.write m4a +"\n"
+
+end
+
+end
+
+puts 'Done!'# your code here

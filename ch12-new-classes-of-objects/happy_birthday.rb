f2260a9b18bd3096819b63a87549e886973f044d
Randy Marmer <rmarmer1@yahoo.com>
2016-05-18 19:46:42 +0100

    Ch12; 'happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,25 @@
-# your code here
+puts "When's your birthday?!?!?"
+
+puts 'Enter the year were you born:'
+
+year = gets.chomp.to_i
+
+puts 'And what month was that? (in numbers: 1-12)'
+
+month = gets.chomp.to_i
+
+puts 'And which day of the month?'
+
+day = gets.chomp.to_i
+
+age = ((Time.now - Time.new(year,month,day)) / 31556926).to_i
+
+# number of seconds in a year = 31556926
+
+while age > 0
+
+puts 'SPANK!'
+
+age -= 1
+
+end
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,6 +1,6 @@
-seconds_alive = Time.now - Time.new(1956,11,28,0,0,0)
+seconds_alive = Time.now - Time.new(1956,11,28)
 
-puts seconds_alive.to_i
+puts seconds_alive
 # number of seconds in a year = 31556926
 age_at_1B_secs = (seconds_alive.to_i - 1000000000) / 31556926
 

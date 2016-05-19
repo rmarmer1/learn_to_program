80f75dbe23ef8f560b071c2af5d57ca32fef287d
Randy Marmer <rmarmer1@yahoo.com>
2016-05-19 16:46:39 +0100

    Ch12; 'birthday_helper.rb - Work-in-progress

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
-# your code here
+names_bdays = {}
+
+name = ""
+mth_day = ""
+year = ""
+
+
+File.readlines('names.txt').each do |line|
+	name, mth_day, year = line.split(",")
+end
+
+names_bdays[name] = "#{mth_day}, #{year}"
+#puts names_bdays
+
+puts 'Enter a name & I\'ll give you their birthday...'
+
+thename = gets.chomp
+
+puts names_bdays[thename]

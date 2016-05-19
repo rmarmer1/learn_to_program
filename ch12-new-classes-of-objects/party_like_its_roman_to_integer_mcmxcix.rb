16d623ae62a9a9ace6bc49fdf27fb9dc55b8c5e2
Randy Marmer <rmarmer1@yahoo.com>
2016-05-19 13:59:56 +0100

    Ch12; 'party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,48 @@
 def roman_to_integer roman
-  # your code here
+
+roman_arab = {'i' => 1, 'v' => 5, 'x' => 10, 'l' => 50, 'c' => 100, 
+							'd' => 500, 'm' => 1000}
+
+arab_tot = 0
+arab_prev = 0
+arab = 0
+
+	roman.reverse.each_char do |rome| 
+
+		if !roman_arab.has_key?(rome.downcase)
+ 
+ 			puts 'This is not a valid roman numeral!'
+
+			return
+#
+		end
+
+		arab = roman_arab[rome.downcase]
+
+		puts "cur arab: #{arab}"
+
+		if arab < arab_prev
+			puts "arab < arab_prev: #{arab} is < #{arab_prev}"
+			arab = arab * -1
+
+		else
+			arab_prev = arab
+			puts "arab_prev = arab: #{arab_prev}"
+		end
+		
+		arab_tot += arab
+		puts "arab_tot: #{arab_tot}"
+	end
+	arab_tot
+
+#puts arab
+
 end
+
+puts roman_to_integer('mcmxcix')	# => 1999
+
+#puts roman_to_integer('CCCLXV')		# => 365
+
+
+
+# 365
